c1e2206ec6feb2fdd6ec3744b983e32056ebafa8
HugoABSSMendes <hugo_skm@outlook.com>
2026-01-06 23:24:48 +0000

    Criação dos Forms.cs

---
+++ Sistema_Gestao_Reservas_Salas_Reuniao/GestaoFuncionariosForm.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace Sistema_Gestao_Reservas_Salas_Reuniao
+{
+    partial class GestaoFuncionariosForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GestaoFuncionariosForm));
+            SuspendLayout();
+            // 
+            // GestaoFuncionariosForm
+            // 
+            AutoScaleDimensions = new SizeF(9F, 18F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.AppWorkspace;
+            ClientSize = new Size(1028, 540);
+            Font = new Font("Arial", 11.25F, FontStyle.Bold);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "GestaoFuncionariosForm";
+            Text = "GestaoFuncionariosForm";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
